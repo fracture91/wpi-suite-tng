f4cc1f9282b0571790ad250d4a23359ff9d4eddb
Andrew Hurle <fracture91@gmail.com>
2012-09-24 16:29:56 -0400

    Added some buttons to the ribbon and more tabs

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/views/MainTabView.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/views/MainTabView.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs.wpisuitetng.modules.defecttracker.views;
 
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 /**
@@ -15,6 +16,8 @@ public class MainTabView extends JTabbedPane {
 		setTabLayoutPolicy(SCROLL_TAB_LAYOUT);
 		addTab("Dashboard", new ImageIcon(), new DashboardView(),
 		       "Your Dashboard - notifications, etc.");
+		addTab("Search", new ImageIcon(), new JPanel(), "Search");
+		addTab("#113", new ImageIcon(), new JPanel(), "Defect #113");
 	}
 	
 }
