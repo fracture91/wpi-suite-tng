736050ed91fd70995316638c6dace2cb35a7ad1b
Mike Della Donna <mpdelladonna@gmail.com>
2012-09-26 00:00:26 -0400

    Moved core files into core directory

---
+++ Core/WPISuite/src/wpisuite/models/Project.java
@@ -0,0 +1,61 @@
+package wpisuite.models;
+
+import com.google.gson.Gson;
+
+public class Project 
+{
+	private String name;
+	private int idNum;
+	
+	public Project(String name, int idNum)
+	{
+		this.name = name;
+		this.idNum = idNum;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getIdNum()
+	{
+		return idNum;
+	}
+	
+	public String toJSON()
+	{
+		String json;
+		
+		Gson gson = new Gson();
+		
+		json = gson.toJson(this, Project.class);
+		return json;
+		
+	}
+	
+	public static String toJSON(Project[] u)
+	{
+		String json ="";
+		
+		Gson gson = new Gson();
+		
+		json = gson.toJson(u, Project[].class);
+		
+		/*
+		for(Project a : u)
+		{
+			json += gson.toJson(a, Project.class);
+		}
+		
+		/*
+		json = "{";
+		json += "name:"+ name +",";
+		json += "idNum:"+ idNum;
+		json += "}";
+		*/
+		
+		return json;
+		
+	}
+}
