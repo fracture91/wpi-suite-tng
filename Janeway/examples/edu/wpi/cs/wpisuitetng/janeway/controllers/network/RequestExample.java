635bbae30ed9d38310c3e1b70e6d46b98d9040bc
JPage <chrispage@wpi.edu>
2012-10-01 16:26:20 -0400

    Updated RequestExample and classpath.

--- Janeway/examples/edu/wpi/cs/wpisuitetng/janeway/controllers/network/RequestExample.java
+++ Janeway/examples/edu/wpi/cs/wpisuitetng/janeway/controllers/network/RequestExample.java
@@ -3,6 +3,8 @@ package edu.wpi.cs.wpisuitetng.janeway.controllers.network;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Defect;
+
 /**
  * This is a preliminary example for making request using the Request class.
  * @author jenny
@@ -15,8 +17,12 @@ public class RequestExample {
 			// Make a new instance of the MyRequestObserver class.
 			final MyRequestObserver requestObserver = new MyRequestObserver();
 			
+			Defect defect = new Defect(1, "This is the description of a defect", "A title", "JJ");
+			
+			String host = "http://localhost:8080/";
+			
 			// Make a new Request. TODO fix this so that the body can be ignored.
-			Request request = new Request(new URL("http://google.com"), Request.RequestMethod.GET, null);
+			Request request = new Request(new URL(host), Request.RequestMethod.GET, defect.toJSON());
 			
 			// Add the requestObserver to the request's set of Observers
 			request.addObserver(requestObserver);
