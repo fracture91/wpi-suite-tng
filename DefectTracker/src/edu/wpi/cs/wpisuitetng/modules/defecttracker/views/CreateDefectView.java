db15e9b9be18d9648158dc3a74203fd8a045d545
Andrew Hurle <fracture91@gmail.com>
2012-10-01 23:13:35 -0400

    Make the Save Changes button work and send a Defect model over the network

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/views/CreateDefectView.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/views/CreateDefectView.java
@@ -3,6 +3,11 @@ package edu.wpi.cs.wpisuitetng.modules.defecttracker.views;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -11,18 +16,25 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
+import edu.wpi.cs.wpisuitetng.janeway.controllers.network.MyRequestObserver;
+import edu.wpi.cs.wpisuitetng.janeway.controllers.network.Request;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Defect;
+
 /**
  * This view is responsible for showing the form for creating a new defect. 
  */
 @SuppressWarnings("serial")
-public class CreateDefectView extends JPanel implements IRibbonButtonProvider {
+public class CreateDefectView extends JPanel implements IRibbonButtonProvider, ActionListener {
 
 	private JPanel buttonPanel;
+	private JButton saveButton;
+	private JTextField titleInput;
 	
 	public CreateDefectView() {
 		buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 0));
-		JButton saveChanges = new JButton("Save Changes");
-		buttonPanel.add(saveChanges);
+		saveButton = new JButton("Save Changes");
+		saveButton.addActionListener(this);
+		buttonPanel.add(saveButton);
 		
 		Box mainPanel = Box.createVerticalBox();
 		JLabel title = new JLabel("Create Defect");
@@ -35,8 +47,10 @@ public class CreateDefectView extends JPanel implements IRibbonButtonProvider {
 		fieldWrapper.setBorder(BorderFactory.createLineBorder(fieldWrapper.getBackground().darker(), 1));
 		fieldWrapper.setMaximumSize(new Dimension(800, 0));
 		fieldWrapper.add(new JLabel("Title:"));
-		fieldWrapper.add(new JTextField(50));
-		// these fields should probably get generated magically from the model in the future
+		titleInput = new JTextField(50);
+		fieldWrapper.add(titleInput);
+		// these fields should probably get generated magically from the custom fields
+		// in the model at some point
 		mainPanel.add(fieldWrapper);
 		
 		mainPanel.add(Box.createVerticalGlue());
@@ -49,6 +63,30 @@ public class CreateDefectView extends JPanel implements IRibbonButtonProvider {
 	@Override
 	public JPanel getButtonPanel() {
 		return buttonPanel;
+	}
+	
+	/**
+	 * Create a Defect model based on the input in the fields and send it to the server.
+	 */
+	private void createAndSendDefect() {
+		// this just prints to the console to confirm everything went through properly
+		final MyRequestObserver requestObserver = new MyRequestObserver();
+		Defect defect = new Defect(1, "This is a description", titleInput.getText(), "SomeUser");
+		try {
+			URL host = new URL("http://localhost:8080/");
+			Request request = new Request(host, Request.RequestMethod.POST, defect.toJSON());
+			request.addObserver(requestObserver);
+			request.send();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == saveButton) {
+			createAndSendDefect();
+		}
 	}
 
 }
