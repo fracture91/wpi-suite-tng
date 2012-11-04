93bc216b6fc8f8b9c3cf9b518e1705f05d42cd80
JPage <chrispage@wpi.edu>
2012-11-04 15:52:14 -0500

    fixed RequestExample

--- Janeway/examples/edu/wpi/cs/wpisuitetng/janeway/network/RequestExample.java
+++ Janeway/examples/edu/wpi/cs/wpisuitetng/janeway/network/RequestExample.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import edu.wpi.cs.wpisuitetng.janeway.network.Request;
 import edu.wpi.cs.wpisuitetng.janeway.network.RequestFactory;
+import edu.wpi.cs.wpisuitetng.janeway.network.Request.RequestMethod;
 
 /**
  * This is a preliminary example for making request using the Request class.
@@ -36,10 +37,9 @@ public class RequestExample {
 			URL url = new URL("http", host, port, "/myModule/myModel"); //TODO switch to https
 			
 			// Make a new POST Request.
-			Request manualRequest = new Request(url);	// construct the Request
+			Request manualRequest = new Request(url, RequestMethod.POST);	// construct the Request
 			
 			// Configure the request
-			manualRequest.setRequestMethod(Request.RequestMethod.POST);	// set the request method to POST
 			manualRequest.setRequestBody(body);	// set the request body to send to the server
 			manualRequest.addObserver(requestObserver);	// Add the requestObserver to the request's set of Observers
 			
@@ -62,10 +62,9 @@ public class RequestExample {
 			RequestFactory factory = new RequestFactory(host, port);
 			
 			// Make a request using the RequestFactory
-			Request manufacturedRequest = factory.makeRequest("myModule", "myModel");
+			Request manufacturedRequest = factory.makeRequest("myModule", "myModel", Request.RequestMethod.POST);
 			
 			// Configure the Request
-			manufacturedRequest.setRequestMethod(Request.RequestMethod.POST);	// set the request method to POST
 			manufacturedRequest.setRequestBody(body2);	// set the request body to send to the server
 			manufacturedRequest.addObserver(requestObserver);	// Add the requestObserver to the request's set of Observers
 			
