bf47ac7cdf52356c202a5f3d671d85a10f77cb19
Andrew Hurle <fracture91@gmail.com>
2012-11-05 03:15:40 -0500

    Make closable tab components prettier

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/tabs/ClosableTabComponent.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/tabs/ClosableTabComponent.java
@@ -1,8 +1,11 @@
 package edu.wpi.cs.wpisuitetng.modules.defecttracker.tabs;
 
+import java.awt.FlowLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,20 +25,27 @@ public class ClosableTabComponent extends JPanel implements ActionListener {
 	 * @param tabbedPane  The JTabbedPane this tab component belongs to
 	 */
 	public ClosableTabComponent(JTabbedPane tabbedPane) {
+		super(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		this.tabbedPane = tabbedPane;
+		setOpaque(false);
 		
 		final JButton closeButton = new JButton("X");
+		closeButton.setFont(closeButton.getFont().deriveFont((float) 8));
+		closeButton.setMargin(new Insets(0, 0, 0, 0));
 		closeButton.addActionListener(this);
 		add(closeButton);
 		
-		add(new JLabel() {
+		final JLabel label = new JLabel() {
 			// display the title according to what's set on our JTabbedPane
+			@Override
 			public String getText() {
 				final JTabbedPane tabbedPane = ClosableTabComponent.this.tabbedPane;
 				final int index = tabbedPane.indexOfTabComponent(ClosableTabComponent.this);
 				return index > -1 ? tabbedPane.getTitleAt(index) : "";
 			}
-		});
+		};
+		label.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 0));
+		add(label);
 	}
 
 	@Override
