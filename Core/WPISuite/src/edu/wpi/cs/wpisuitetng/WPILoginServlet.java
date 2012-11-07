7d26bdf40fc1802ba66aa698ceba45b3da9e9349
Mike Della Donna <mpdelladonna@gmail.com>
2012-11-07 17:28:02 -0500

    Added a login servlet with stubs

---
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
@@ -0,0 +1,32 @@
+package edu.wpi.cs.wpisuitetng;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WPILoginServlet
+ */
+@WebServlet("/login")
+public class WPILoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WPILoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
