4bbace8b1d95065755f7f1fb62e6331125e0aefe
Mike Della Donna <mpdelladonna@gmail.com>
2012-11-09 14:03:10 -0500

    Changed the login servlet to send back a test cookie

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
@@ -3,6 +3,7 @@ package edu.wpi.cs.wpisuitetng;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,14 +20,26 @@ public class WPILoginServlet extends HttpServlet {
      */
     public WPILoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
+		Cookie userCookie = new Cookie("user","test");
+		response.addCookie(userCookie);
 	}
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// sends back a test cookie
+		Cookie userCookie = new Cookie("user","test");
+		resp.addCookie(userCookie);
+	}
+	
+	
+	
 }
