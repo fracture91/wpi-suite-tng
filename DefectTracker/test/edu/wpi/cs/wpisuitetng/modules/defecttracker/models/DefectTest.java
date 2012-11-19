aaaec02f50f864eb577f6d0923f6c1643f7cb83d
Andrew Hurle <fracture91@gmail.com>
2012-11-18 22:01:26 -0500

    Change Defect model to have necessary fields discussed earlier Cleaned up some comments

--- DefectTracker/test/edu/wpi/cs/wpisuitetng/modules/defecttracker/models/DefectTest.java
+++ DefectTracker/test/edu/wpi/cs/wpisuitetng/modules/defecttracker/models/DefectTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
 public class DefectTest {
 
-	private final Defect d1 = new Defect(1, "", "", "");
-	private final Defect d1copy = new Defect(1, "", "", "");
-	private final Defect d2 = new Defect(2, "", "", "");
+	private final User bob = new User("Bob", "bob", -1);
+	private final Defect d1 = new Defect(1, "", "", bob);
+	private final Defect d1copy = new Defect(1, "", "", bob);
+	private final Defect d2 = new Defect(2, "", "", bob);
 	
 	@Test
 	public void testIdentify() {
