3963351b182c9d49e91ad0ec6ec8f655a11cf4d0
JPage <chrispage@wpi.edu>
2012-11-27 21:31:58 -0500

    fixed SaveDefectController in DefectTracker

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/create/SaveDefectController.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/create/SaveDefectController.java
@@ -1,6 +1,9 @@
 package edu.wpi.cs.wpisuitetng.modules.defecttracker.create;
 
+import java.net.MalformedURLException;
+
 import edu.wpi.cs.wpisuitetng.janeway.config.ConfigManager;
+import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.Request.RequestMethod;
 
@@ -12,7 +15,7 @@ public class SaveDefectController {
 
 	/** The view object containing the request fields */
 	protected DefectPanel view;
-	
+
 	/**
 	 * Construct a new handler for the given view
 	 * @param view the view containing the request fields
@@ -27,10 +30,21 @@ public class SaveDefectController {
 	public void save() {
 		final SaveRequestObserver requestObserver = new SaveRequestObserver();
 		// TODO Change PUT/POST depending on whether this is create or update
-		Request request = new Request(ConfigManager.getConfig().getCoreUrl(), RequestMethod.PUT);
-		request.setRequestBody(view.getModel().toJSON());
-		request.addObserver(requestObserver);
-		request.send();
+		Request request;
+		try {
+			request = Network.getInstance().makeRequest("defecttracker/defect", RequestMethod.PUT);
+			request.setRequestBody(view.getModel().toJSON());
+			request.addObserver(requestObserver);
+			request.send();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException();
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
 	}
-	
+
 }
