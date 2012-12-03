4ee5d11c65786db9d5fd4f2248ccf29fab1b0373
Tyler Wack <tylerwack@gmail.com>
2012-12-03 01:02:08 -0500

    Added EPL license and contributors section as Header comments in Core project. The header was applied each file in all packages except for wpisuitetng.database

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2012 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    mpdelladonna
+ *    twack
+ *******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng;
 
 import java.io.BufferedReader;
@@ -18,16 +31,20 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 /**
  * Servlet implementation class WPILoginServlet
+ * 
+ * @author mpdelladonna, twack
  */
 @WebServlet("/login")
 public class WPILoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Authenticator auth;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public WPILoginServlet() {
         super();
+        
     }
 
 	/**
