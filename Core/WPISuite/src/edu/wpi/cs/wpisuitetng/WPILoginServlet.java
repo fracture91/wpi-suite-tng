0ecea938832105bf76df8713a458f351907cbac0
Tyler Wack <tylerwack@gmail.com>
2012-12-03 15:10:10 -0500

    Added Authentication abstraction and BasicAuth implementation. Refactored the Login Servlet to use the new Authenticator for login and logout. Needs to be unit tested

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/WPILoginServlet.java
@@ -15,9 +15,6 @@ package edu.wpi.cs.wpisuitetng;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -44,7 +41,7 @@ public class WPILoginServlet extends HttpServlet {
      */
     public WPILoginServlet() {
         super();
-        
+        this.auth = new BasicAuth(); // define Authorization implementation
     }
 
 	/**
@@ -52,23 +49,27 @@ public class WPILoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// parse the POST body into a username:password string
+		// parse the POST body into a String
 		BufferedReader in = request.getReader();
-		String[] decoded = decodeAuthToken(in); // String array [<username>, <password>]
-		
-		// TODO: find a way to avoid the BASIC_AUTH-specific 'username:password' decoded format.
+		String postString = new String();
+		String line = in.readLine();
+		while(line != null)
+		{
+			postString.concat(line);
+			line = in.readLine();
+		}
 		
 		// Authentication
 		try 
 		{	
-			Session ses = loginUser(decoded[0], decoded[1]);
+			Session ses = this.auth.login(postString);
 			
 			// post back the Session Cookie.
 			Cookie userCookie = ses.toCookie();
 			response.addCookie(userCookie);
 			response.setStatus(HttpServletResponse.SC_CONTINUE);  //100 - Client can continue
 		}
-		catch(WPISuiteException e) // Authentication Failed.
+		catch(AuthenticationException e) // Authentication Failed.
 		{
 			//TODO: log error
 			response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403 - Forbidden, Authentication Failed.
@@ -98,68 +99,11 @@ public class WPILoginServlet extends HttpServlet {
 				// if found, then logout the user with the given username.
 				if(cookies[cookieIndex].getName().startsWith("WPISUITE-"))
 				{
-					this.logoutUser(cookies[cookieIndex].getValue()); // logs out the user with the given session cookie.
+					this.auth.logout(cookies[cookieIndex].getValue()); // logs out the user with the given session cookie.
 					resp.setStatus(HttpServletResponse.SC_CONTINUE); // logout successful
 					found = true;
 				}
 			}
 		}
-	}
-	
-	/**
-	 * Performs user logout logic. Removes the Session for the given sessionToken.
-	 * @param session
-	 */
-	private void logoutUser(String session)
-	{
-		ManagerLayer manager = ManagerLayer.getInstance();
-		manager.getSessions().removeSession(session);
-	}
-
-	/**
-	 * Evaluates user authentication credentials.
-	 * 	Creates and returns a Session if authentication is successful
-	 * @param username
-	 * @param password
-	 * @return	the Session for this user.
-	 */
-	private Session loginUser(String username, String password) throws WPISuiteException
-	{
-		ManagerLayer manager = ManagerLayer.getInstance();
-		User[] u = manager.getUsers().getEntity(username);
-		
-		if(u.length == 0)
-		{
-			throw new AuthenticationException();	//"No user with the given username found");
-		}
-
-		User user = u[0];
-		
-		if(!user.matchPassword(password))
-		{
-			throw new AuthenticationException();
-		}
-		
-		// create a Session mapping in the ManagerLayer
-		Session ses = manager.getSessions().createSession(user);
-		
-		//TODO: handle Sessions that already exist. Renewing sessions, etc.
-		
-		return ses;
-	}
-	
-	/**
-	 * Handles Authentication Token decoding.
-	 * 
-	 * @param body	The raw POST body to be parsed. 
-	 * @return	a String array containing login credentials where [0] username, [1] password
-	 */
-	private String[] decodeAuthToken(BufferedReader body)
-	{
-		String[] credentials = {"username", "password"};
-		
-		return credentials;
-	}
-
-	
+	}	
 }
