6d72ba838f6dc3306cef7a9e38e88911f6c5d4ad
JPage <chrispage@wpi.edu>
2012-12-09 15:11:35 -0500

    removed superfluous imports

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/defect/DefectPanel.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/defect/DefectPanel.java
@@ -1,8 +1,6 @@
 package edu.wpi.cs.wpisuitetng.modules.defecttracker.defect;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.lang.reflect.InvocationTargetException;
