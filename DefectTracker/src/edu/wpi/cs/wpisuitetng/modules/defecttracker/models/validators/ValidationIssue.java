be7da346bd352301eb5b0641f5a43cba83edf6d7
Andrew Hurle <fracture91@gmail.com>
2013-01-11 00:55:12 -0500

    Move Defect validation to its own class, tests for it

---
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/models/validators/ValidationIssue.java
@@ -0,0 +1,51 @@
+package edu.wpi.cs.wpisuitetng.modules.defecttracker.models.validators;
+
+/**
+ * Represents an error in model validation. 
+ */
+public class ValidationIssue {
+	
+	private String message;
+	private String fieldName;
+	
+	/**
+	 * Create a ValidationIssue caused by a particular field in the model.
+	 * 
+	 * @param message An error message ("Must be 5-100 characters")
+	 * @param fieldName The relevant field name ("title")
+	 */
+	public ValidationIssue(String message, String fieldName) {
+		this.message = message;
+		this.fieldName = fieldName;
+	}
+	
+	/**
+	 * Create a generic ValidationIssue with no specific field at fault
+	 * @param message An error message ("You are not allowed to edit defects")
+	 */
+	public ValidationIssue(String message) {
+		this(message, null);
+	}
+
+	/**
+	 * @return the message associated with this error
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @return the fieldName causing this error
+	 */
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	/**
+	 * @return true if this error has a fieldName
+	 */
+	public boolean hasFieldName() {
+		return fieldName != null;
+	}
+	
+}
---
+++ DefectTracker/test/edu/wpi/cs/wpisuitetng/modules/defecttracker/models/validators/DefectValidatorTest.java
@@ -0,0 +1,87 @@
+package edu.wpi.cs.wpisuitetng.modules.defecttracker.models.validators;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import edu.wpi.cs.wpisuitetng.database.Data;
+import edu.wpi.cs.wpisuitetng.modules.Model;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.defect.DefectPanel.Mode;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Defect;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.DefectEvent;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.DefectStatus;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Tag;
+
+public class DefectValidatorTest {
+
+	Defect existingDefect;
+	Defect goodNewDefect;
+	Defect goodUpdatedDefect;
+	User bob;
+	User bobCopy;
+	Tag tag;
+	Tag tagCopy;
+	List<DefectEvent> ignoredEvents;
+	Data db;
+	DefectValidator validator;
+	
+	@Before
+	public void setUp() {
+		tag = new Tag("tag");
+		bob = new User("bob", "bob", "1234", 1);
+		existingDefect = new Defect(1, "An existing defect", "", bob);
+		existingDefect.setCreationDate(new Date());
+		existingDefect.setLastModifiedDate(new Date());
+		existingDefect.setEvents(new ArrayList<DefectEvent>());
+		
+		//need copies to simulate db4o cross-container problem
+		tagCopy = new Tag("tag");
+		bobCopy = new User(null, "bob", null, -1);
+		goodNewDefect = new Defect(-1, "This is a good title", "", bobCopy);
+		goodNewDefect.setAssignee(bobCopy);
+		goodNewDefect.getTags().add(tagCopy);
+		goodNewDefect.setStatus(DefectStatus.CONFIRMED); // ignored
+		ignoredEvents = new ArrayList<DefectEvent>();
+		goodNewDefect.setEvents(ignoredEvents); // ignored
+		
+		Set<Model> models = new HashSet<Model>();
+		models.add(tag);
+		models.add(bob);
+		models.add(existingDefect);
+		db = new MockDefectData(models);
+		validator = new DefectValidator(db);
+	}
+	
+	@Test
+	public void testDBState() {
+		assertSame(tag, db.retrieve(Tag.class, "name", "tag").get(0));
+		assertSame(bob, db.retrieve(User.class, "username", "bob").get(0));
+		assertSame(existingDefect, db.retrieve(Defect.class, "id", 1).get(0));
+	}
+	
+	@Test
+	public void testGoodNewDefect() {
+		List<ValidationIssue> issues = validator.validate(goodNewDefect, Mode.CREATE);
+		assertEquals(issues.size(), 0);
+		assertSame(bob, goodNewDefect.getAssignee());
+		assertSame(bob, goodNewDefect.getCreator());
+		for(Tag t : goodNewDefect.getTags()) {
+			assertSame(tag, t);
+		}
+		assertEquals(DefectStatus.NEW, goodNewDefect.getStatus());
+		assertEquals(0, goodNewDefect.getEvents().size());
+		assertNotSame(ignoredEvents, goodNewDefect.getEvents());
+		assertNotNull(goodNewDefect.getCreationDate());
+		assertNotNull(goodNewDefect.getLastModifiedDate());
+	}
+
+}
