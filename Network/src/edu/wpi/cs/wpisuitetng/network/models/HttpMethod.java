26c05086975ab932e70158f1362e4ec78688c714
JPage <chrispage@wpi.edu>
2013-01-20 13:50:51 -0500

    Modified HttpMethod - removed unnecessary toString method - added javadoc comment for the enum

--- Network/src/edu/wpi/cs/wpisuitetng/network/models/HttpMethod.java
+++ Network/src/edu/wpi/cs/wpisuitetng/network/models/HttpMethod.java
@@ -1,12 +1,13 @@
 package edu.wpi.cs.wpisuitetng.network.models;
 
+/**
+ * Represents the GET, POST, PUT and DELETE HTTP methods.
+ * 
+ * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
+ */
 public enum HttpMethod {
 	GET, 
 	POST, 
 	PUT, 
 	DELETE;
-	
-	public String toString() {
-		return this.name();
-	}
 }
