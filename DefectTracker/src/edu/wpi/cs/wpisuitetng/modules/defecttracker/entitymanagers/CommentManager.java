1df70e1377befd14016ece129807f03027d4eeac
Andrew Hurle <fracture91@gmail.com>
2013-01-27 16:11:11 -0500

    Add CommentValidator, tests.  Fix misc warnings.

--- DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/entitymanagers/CommentManager.java
+++ DefectTracker/src/edu/wpi/cs/wpisuitetng/modules/defecttracker/entitymanagers/CommentManager.java
@@ -1,102 +1,104 @@
 package edu.wpi.cs.wpisuitetng.modules.defecttracker.entitymanagers;
 
+import java.util.List;
+
 import com.google.gson.Gson;
 
 import edu.wpi.cs.wpisuitetng.Session;
 import edu.wpi.cs.wpisuitetng.database.Data;
 import edu.wpi.cs.wpisuitetng.exceptions.BadRequestException;
-import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
-import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
-import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
+import edu.wpi.cs.wpisuitetng.exceptions.NotImplementedException;
 import edu.wpi.cs.wpisuitetng.modules.EntityManager;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Comment;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.Defect;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.validators.CommentValidator;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.models.validators.ValidationIssue;
 
+/**
+ * Responsible for handling requests that interact with the Comment model.
+ */
 public class CommentManager implements EntityManager<Comment> {
 
-	private Data db;
-	private Gson gson;
-	private DefectManager defectManager;
+	private final Data db;
+	private final Gson gson;
+	private final CommentValidator validator;
 	
+	/**
+	 * Create a CommentManager that uses the given Data instance
+	 * @param data The Data instance to interact with
+	 */
 	public CommentManager(Data data) {
 		db = data;
 		gson = new Gson();
-		defectManager = new DefectManager(data);
+		validator = new CommentValidator(data);
 	}
 	
 	@Override
-	public Comment makeEntity(Session s, String content)
-			throws BadRequestException, ConflictException, WPISuiteException {
+	public Comment makeEntity(Session s, String content) throws BadRequestException {
 		Comment newComment = gson.fromJson(content, Comment.class);
-		Defect[] defects = defectManager.getEntity(s, Integer.toString(newComment.getDefectId()));
-		defects[0].getEvents().add(newComment);
-		db.save(defects[0]);
+		
+		List<ValidationIssue> issues = validator.validate(s, newComment);
+		if(issues.size() > 0) {
+			// TODO: pass errors to client through exception
+			throw new BadRequestException();
+		}
+		
+		Defect defect = validator.getLastExistingDefect();
+		defect.getEvents().add(newComment);
+		db.save(defect.getEvents());
+		
 		System.out.println("Created Comment");
 		return newComment;
 	}
 
 	@Override
-	public Comment[] getEntity(Session s, String id) throws NotFoundException,
-			WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public Comment[] getEntity(Session s, String id) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public Comment[] getAll(Session s) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public Comment[] getAll(Session s) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public Comment update(Session s, String content) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public Comment update(Session s, String content) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public void save(Session s, Comment model) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		
+	public void save(Session s, Comment model) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public boolean deleteEntity(Session s, String id) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean deleteEntity(Session s, String id) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public String advancedGet(Session s, String[] args)
-			throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public String advancedGet(Session s, String[] args) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public void deleteAll(Session s) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		
+	public void deleteAll(Session s) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public int Count() throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int Count() throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public String advancedPut(Session s, String[] args, String content)
-			throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public String advancedPut(Session s, String[] args, String content) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 	@Override
-	public String advancedPost(Session s, String string, String content)
-			throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+	public String advancedPost(Session s, String string, String content) throws NotImplementedException {
+		throw new NotImplementedException();
 	}
 
 }
