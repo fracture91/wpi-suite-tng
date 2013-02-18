9e464616c4120e6b538a4a3e866e2e2711416adf
bgaffey <gaffey.brian@gmail.com>
2013-02-17 22:57:11 -0500

    Javadoc all methods in the Data interface.
    Brian Gaffey

--- Core/WPISuite-Interfaces/src/edu/wpi/cs/wpisuitetng/database/Data.java
+++ Core/WPISuite-Interfaces/src/edu/wpi/cs/wpisuitetng/database/Data.java
@@ -42,7 +42,7 @@ public interface Data
 	 * @param anObjectQueried - class of object to be retrieved
 	 * @param aFieldName - field that object will be retrieved by
 	 * @param theGivenValue - value of aFieldName
-	 * @return
+	 * @return List<Model> - list of the models matching the given criteria
 	 * @throws WPISuiteException 
 	 */
 	public List<Model> retrieve(@SuppressWarnings("rawtypes") final Class anObjectQueried, String aFieldName, final Object theGivenValue) throws WPISuiteException;
@@ -55,18 +55,18 @@ public interface Data
 	 * @param aFieldName - field that object will be retrieved by
 	 * @param theGivenValue - value of aFieldName
 	 * @param theProject - what Project to search in 
-	 * @return
+	 * @return List<Model> - list of the models matching the given criteria
 	 * @throws WPISuiteException 
 	 */
 	public List<Model> retrieve(@SuppressWarnings("rawtypes") final Class anObjectQueried, String aFieldName, final Object theGivenValue, final Project theProject) throws WPISuiteException;
 	
 	
 	/**
-	 * Deletes aTNG from the database
-	 * @param aTNG
+	 * Deletes aModel from the database
+	 * @param aModel the object to be deleted
 	 * @return the object that was deleted
 	 */
-	public <T> T delete(T aTNG);
+	public <T> T delete(T aModel);
 	
 	/**
 	 * Updates an object in the database.  
@@ -77,6 +77,7 @@ public interface Data
 	 * @param changeValue - Value that changeField will be changed to
 	 * @throws WPISuiteException 
 	 */
+	@SuppressWarnings("rawtypes")
 	public void update(final Class anObjectToBeModified, String fieldName, Object uniqueID, 
 			String changeField, Object changeValue) throws WPISuiteException;
 	
@@ -89,6 +90,7 @@ public interface Data
 	 * @param changeValue - Value that changeField will be changed to
 	 * @throws WPISuiteException 
 	 */
+	@SuppressWarnings("rawtypes")
 	public void update(final Class anObjectToBeModified, String fieldName, Object uniqueID, 
 			String changeField, Object changeValue, Project aProject) throws WPISuiteException;
 	
@@ -122,21 +124,102 @@ public interface Data
 	 */
 	public <T> List<Model> deleteAll(T aSample, Project aProject);
 	
+	/**
+	 * Retrieves a list of all the models matching all the given fields to the given value in the database
+	 * @param anObjectQueried - Class of the object to be retrieved
+	 * @param aFieldNameList - An Array of Strings matching the names of the fields to check against
+	 * @param theGivenValueList - An Array of values to compare the fields to. These must appear in the same order
+	 * @return List of all the models matching all of the given the fields and values
+	 * @throws WPISuiteException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	List<Model> andRetrieve(final Class anObjectQueried, String[] aFieldNameList, 
 			final List<Object> theGivenValueList) throws WPISuiteException, IllegalArgumentException, IllegalAccessException, InvocationTargetException;
 	
+	/**
+	 * Retrieves a list of all the models matching all the given fields to the given value in the database
+	 * @param anObjectQueried - Class of the object to be retrieved
+	 * @param aFieldNameList - An Array of Strings matching the names of the fields to check against
+	 * @param theGivenValueList - An Array of values to compare the fields to. These must appear in the same order
+	 * @param aProject - The project that all returned objects must be from
+	 * @return List of all the models matching all of the given the fields and values in the given project
+	 * @throws WPISuiteException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	List<Model> andRetrieve(final Class anObjectQueried, String[] aFieldNameList, 
 			final List<Object> theGivenValueList, final Project aProject) throws WPISuiteException, IllegalArgumentException, IllegalAccessException, InvocationTargetException;
 	
+	/**
+	 * Retrieves a list of all the models matching one of the given fields to the given value in the database
+	 * @param anObjectQueried - Class of the object to be retrieved
+	 * @param aFieldNameList - An Array of Strings matching the names of the fields to check against
+	 * @param theGivenValueList - An Array of values to compare the fields to. These must appear in the same order
+	 * @return List of all the models matching one of the given the fields and values
+	 * @throws WPISuiteException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	List<Model> orRetrieve(final Class anObjectQueried, String[] aFieldNameList, final List<Object> theGivenValueList) throws WPISuiteException, IllegalAccessException, InvocationTargetException;
 	
+	/**
+	 * Retrieves a list of all the models matching one of the given fields to the given value in the database
+	 * @param anObjectQueried - Class of the object to be retrieved
+	 * @param aFieldNameList - An Array of Strings matching the names of the fields to check against
+	 * @param theGivenValueList - An Array of values to compare the fields to. These must appear in the same order
+	 * @param aProject - The project that all returned objects must be from
+	 * @return List of all the models matching one of the given the fields and values in the given project
+	 * @throws WPISuiteException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	List<Model> orRetrieve(final Class anObjectQueried, String[] aFieldNameList, 
 			final List<Object> theGivenValueList, final Project aProject) throws WPISuiteException, IllegalAccessException, InvocationTargetException;
 	
+	/**
+	 * Retrieves a list of all models matching all of the given fields to the given values of the "And"s 
+	 * and matching one of the given fields to the given values of the "Or"s in the database
+	 * @param andanObjectQueried - Class of the object to be queried with the and
+	 * @param andFieldNameList - The names of the fields to be queried with an and
+	 * @param andGivenValueList - The values of the fields to be queried with an and
+	 * @param orAnObjectQueried - Class of the object to be queried with an or
+	 * @param orFieldNameList - The names of the fields to be queried with an or
+	 * @param orGivenValueList - the values of the fields to be queried with an or
+	 * @return A list of models that match all of the and values and at least 1 of the or values 
+	 * @throws WPISuiteException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	public List<Model> complexRetrieve(final Class andanObjectQueried, String[] andFieldNameList, 
 			final List<Object> andGivenValueList, final Class orAnObjectQueried, String[] orFieldNameList, 
 			final List<Object> orGivenValueList) throws WPISuiteException, IllegalArgumentException, IllegalAccessException, InvocationTargetException;
-
+	
+	/**
+	 * Retrieves a list of all models matching all of the given fields to the given values of the "And"s 
+	 * and matching one of the given fields to the given values of the "Or"s in the database from the given project
+	 * @param andanObjectQueried - Class of the object to be queried with the and
+	 * @param andFieldNameList - The names of the fields to be queried with an and
+	 * @param andGivenValueList - The values of the fields to be queried with an and
+	 * @param orAnObjectQueried - Class of the object to be queried with an or
+	 * @param orFieldNameList - The names of the fields to be queried with an or
+	 * @param orGivenValueList - the values of the fields to be queried with an or
+	 * @param aProject - The project that all matching objects must come from
+	 * @return A list of models that match all of the and values and at least 1 of the or values from the given project
+	 * @throws WPISuiteException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 */
+	@SuppressWarnings("rawtypes")
 	public List<Model> complexRetrieve(final Class andanObjectQueried, String[] andFieldNameList, 
 			final List<Object> andGivenValueList, final Class orAnObjectQueried, String[] orFieldNameList, 
 			final List<Object> orGivenValueList, final Project aProject) throws WPISuiteException, IllegalArgumentException, IllegalAccessException, InvocationTargetException;
