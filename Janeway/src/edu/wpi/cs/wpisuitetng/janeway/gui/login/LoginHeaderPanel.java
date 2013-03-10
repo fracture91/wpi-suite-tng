f1ad3110d7810e2cd2f7819ec26ff385a66e9b92
Andrew Hurle <fracture91@gmail.com>
2013-03-10 01:38:35 -0500

    Add EPL headers to DefectTracker, Network, Janeway, PostBoard

--- Janeway/src/edu/wpi/cs/wpisuitetng/janeway/gui/login/LoginHeaderPanel.java
+++ Janeway/src/edu/wpi/cs/wpisuitetng/janeway/gui/login/LoginHeaderPanel.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Chris Casola
+ ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.janeway.gui.login;
 
 import java.awt.Dimension;
